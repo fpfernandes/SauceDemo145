912b47ee4f462c93983d3828681ac7d44c29f306
fpfernandes <fabianapresoto@gmail.com>
2025-09-29 22:11:19 -0300

    path tools

---
+++ Utils/path_tools.js
@@ -0,0 +1,57 @@
+// snapshot = print --> queremos quando passa e quando falha
+// Varios prints por execucao
+// Criar uma pasta chamada Snapshot e colocar todos os prints dentro 
+// Organize as datas no formato universal ano-mes-dia
+
+// Bibliotecas
+const fs = require('fs') // File system / biblioteca do sistema operacional
+const path = require('path') // biblioteca de caminhos de pastas/arquivos
+
+// Formatar numeros com zero na frente se precisar
+function pad2(num) {return String(num).padStart(2, '0')
+}
+
+// Funcao para definir data e hora baseado no momento da execucao 
+function compute_run_folder(baseDir){
+    // Cria o carimbo de data via CI(Integracao Continua)
+    if (process.env.RUN_TAG){ // == true
+        const tag = process.env.RUN_TAG.replace(/[^\w-:.]/g, '_') //CI
+        const runDir = path.join(baseDir, tag)
+        fs.mkdirSync(runDir, {recursive: true})
+        return runDir
+    }
+
+    // Verifica data e hora
+    const now = new Date() // perguntar para o computador que dia e que horas sao
+    const yyyy = now.getFullYear()    // Ano com 4 digitos
+    const MM = pad2(now.getMonth())   // Mes com dois digitos
+    const dd = pad2(now.getDate())    // Dia com dois digitos
+    const HH = pad2(now.getHours())   // Hora com dois digitos
+    const mm = pad2(now.getMinutes())  // Minutos com dois digitos
+    const ss = pad2(now.getSeconds())  // Segundos com 2 digitos
+        
+    // const Mes = String(now.getMonth()).padStart(2, '0') // outro jeito de fazer o mes formatado
+    // const Dia = String(now.getDate()).padStart(2, '0') // outro jeito de fazer o dia formatado
+
+    // Criar pastas 
+    const runDir = path.join(baseDir, `${yyyy}`, `${MM}`, `${dd}`, `${HH}-${mm}-${ss}`)
+    fs.mkdirSync(runDir, {recursive: true})
+    return runDir
+
+}
+
+// Criar subpastas dentro da estrutura de datas e horas
+function ensure_subdirs(runDir){
+    const dirs = {
+        runDir,
+        screenshotsDir: path.join(runDir, 'screenshots')
+    }
+
+    Object.values(dirs).forEach(d => {  // d = diretorio (subdiretorio)
+        if (!fs.existsSync(d)) fs.mkdirSync(d, {recursive: true})
+    })
+    return dirs
+
+}
+
+module.exports = { computer_run_folder, ensure_subdirs }
--- tests/comprar_mochila.spec.js
+++ tests/comprar_mochila.spec.js
@@ -1,56 +1,61 @@
-// 1 - Referencias e bibliotecas
-// Declara um objeto chamado test vindo da biblioteca Playwright 
-const { test, expect } = require('playwright/test')
-
-// 2 - Classe ou Funcoes ou Metodos
+// 1 - Referência e bibliotecas
+// Declara um objeto chamado test vindo da biblioteca Playwrigth
+const { test, expect } = require('@playwright/test')
+ 
+// 2 - Classe ou Funções ou Métódos
 // Um script pode executar de forma:
-// - Sincrona: Simultaneo. Ex: Ligacao de voz
-// - Assincrona: Separados. Ex: Mensagem de texto no whatsapp
+// - Sincrona: Simultâneo. Ex.: ligação de voz
+// - Assincrona: Separados. Ex.: mensagem de texto no WhatsApp
 test('Realizar o fluxo de compra da mochila', async ({page}) => {
-    await page.goto('https://www.saucedemo.com') // abre o browser no site alvo
-    await expect(page).toHaveURL('https://www.saucedemo.com')// verifica se esta na pagina raiz 
+ 
+    await page.goto('/') // abre o browser no site alvo
+    await expect(page).toHaveURL('/')            // verifica se está na página raiz
     const botao_login = page.locator('#login-button')
-    await expect(botao_login).toHaveText('Login') // verifica elemento escrito login
-
-    // Pagina inicial Realizar o login
+    await expect(botao_login).toHaveText('Login') // verifica elemento escrito Login
+   
+    // Página Inicial Realizar o login
     // Preencher o campo cujo localizador é name com o valor standard_user
     await page.fill('[name="user-name"]', 'standard_user')
-    // Preencher o campo cujo localizador é um css selector com o valor secret_sauce
-    await page.fill('[name="password"]', 'secret_sauce')
-    // Clicar no botao login
+    // Preencher o campo cujo localizador é cssSelector com o valor secret_sauce
+    await page.fill('[placeholder="Password"]', 'secret_sauce')
+    // Clicar no botão Login
     await botao_login.click()
-
-    // Pagina de Inventario / Produtos
-    // Verificar se esta na pagina certa
+ 
+    // Página de Inventário / Produtos
+    // Verificar se está na página certa
     await expect(page).toHaveURL(/.*inventory/)
-    // Se precisar mudar de valor 2 ou mais vezes 
-    // Trocar de const (constante) para let (variavel)
+    // Se precisar mudar de valor 2 ou mais vezes
+    // Trocar de const (constante) para let (variável)
     const tituloSecao = '.title' //cssSelector
     await expect(page.locator(tituloSecao)).toHaveText('Products')
-
-    // Adicionar o producto ao carrinho de compras
-    const btnAdicionar = 'xpath=/html/body/div/div/div/div[2]/div/div/div/div[1]/div[2]/div[2]/button' //localizador xpath
+ 
+    // Adicionar a mochila ao carrinho de compras
+    const btnAdicionar = 'xpath=/html/body/div/div/div/div[2]/div/div/div/div[1]/div[2]/div[2]/button'
     await page.locator(btnAdicionar).click()
-
-    // Verificar se exibe o numero um no carrinho de compras
-    // const icoQuantCart = 'span.shopping_cart_badge' //cssSelector
-    const icoQuantCart = '#shopping_cart_container > a' //Selector
+ 
+    // Verificar se exibe o nº 1 no carrinho de compras
+    const icoQuantCart = 'span.shopping_cart_badge' //cssSelector
+    // const icoQuantCart = '#shopping_cart_container > a' //Selector
     await expect(page.locator(icoQuantCart)).toHaveText('1')
-
-    // Clicar no icone do carrinho (n 1)
+ 
+    // Clicar no icone do carrinho (nº 1)
     await page.locator(icoQuantCart).click()
-
+   
     // Verificar se está na página certa - Cart
     await expect(page).toHaveURL(/.*cart/)
-    // tituloSecao permanece igual ao da pagina de Inventory
+    // tituloSecao permanece igual ao da página Inventory
     await expect(page.locator(tituloSecao)).toHaveText('Your Cart')
-
+ 
     // Verificar dados funcionais
     await expect(page.locator('.cart_quantity')).toHaveText('1')
     await expect(page.locator('.inventory_item_name')).toHaveText('Sauce Labs Backpack')
     await expect(page.locator('.inventory_item_price')).toHaveText('$29.99')
-
+ 
     // Espera de 1 segundo
-    await page.waitForTimeout(1000) // mal visto // alfinete //temporario
+    await page.waitForTimeout(1000) // mal visto // alfinete // temporária
+ 
+}) // final do teste
+ 
 
-}) // Final do teste
+ 
+
