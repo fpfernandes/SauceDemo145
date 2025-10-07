a8aa04ebc891c0fb1fbafefc9b2ebfd54b2587cf
fpfernandes <fabianapresoto@gmail.com>
2025-10-07 02:11:14 +0100

    script estruturado ate o passo 3

--- Utils/logger.js
+++ Utils/logger.js
@@ -1,26 +1,25 @@
 const { test: base } = require('@playwright/test')
 const fs = require('fs')
 const path = require('path')
-
-// Formatar espacamento entre datas AM-PM
+ 
+// Formatar espaçamento entre datas AM-PM
 function isoTs() {
     const nova_data = new Date();
     return nova_data.toISOString().replace('T', ' ').replace('Z', '')
-
 }
-
-const LOGS_DIR = process.env.LOGS_DIR || path.join(process.cwd, 'artifacts', 'logs')  // dois pipes || representa OU cwd retorna pasta atual
-if (!fs.existsSync(LOGS_DIR)) fs.mkdirSync(LOGS_DIR, {recursive: true}) // if testa se a pasta existe, se nao cria uma
-
-// cria o arquivo de log para a execucao
+ 
+const LOGS_DIR = process.env.LOGS_DIR || path.join(process.cwd, 'artifacts', 'logs')
+if (!fs.existsSync(LOGS_DIR)) fs.mkdirSync(LOGS_DIR, {recursive: true})
+ 
+// cria o arquivo de log para a execução    
 const EXEC_LOG = path.join(LOGS_DIR, 'steps.log')
-
+ 
 // estrutura para escrever no arquivo de log
 export const test = base.extend({
     log: async ({}, use, testInfo) => {
         function log(message) {
-            // cada linha sera composta por 
-            // {Data/Hora} {Titulo do teste} {Mensagem}
+            // cada linha será composta por
+            // {Data/Hora} {Titulo do Teste} {Mensagem}
             const line = `[${isoTs()}] [${testInfo.title}] ${message}\n`
             fs.appendFileSync(EXEC_LOG, line, 'utf8') // escreve no arquivo
             return line;
@@ -28,7 +27,5 @@ export const test = base.extend({
         await use(log);
     }
 })
-
+ 
 export const expect = base.expect
-
-
--- Utils/snap.js
+++ Utils/snap.js
@@ -1,21 +1,19 @@
 const fs = require('fs')
 const path = require('path')
 
-// constante para armazenar o local onde ficarao gravados os snapshots
+// constante para armazenar o local onde ficarão gravados os snapshops
 const SHOTS_DIR = process.env.SCREENSHOTS_DIR
 
-// garantir que o nome do arquivo seja compativel 
+// garantir que o nome do arquivo seja compátivel
 function safe_name(name){
-    return String(name).replace(/[^\w\d-_.]+/g, '_').slice(0, 120) 
+    return String(name).replace(/[^\w\d-_.]+/g, '_').slice(0, 120)
 }
-
-/**
- * Salvar o screenshot quando solicitado com nome amigavel
+/** 
+ * salvar o screenshot quando solicitado, com nome amigável
  * @param { import('@playwright/test').Page} page
  * @param { import('@playwright/test').TestInfo} test_info
  * @param { string } label
 */
-
 async function snap(page, test_info, label){
     const file = `${safe_name(test_info.title)}__${safe_name(label)}.png`
     const dest = path.join(SHOTS_DIR, file)
@@ -26,4 +24,3 @@ async function snap(page, test_info, label){
 }
 
 module.exports = { snap }
-
